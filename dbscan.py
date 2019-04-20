26adb6c0b0569b5d4eee5c966bf42b7d80e0ef4e
mostafa <mostafa.ahmadi1375@yahoo.com>
2019-04-20 13:12:48 +0430

    dbscan done except for evaluation part

--- dbscan.py
+++ dbscan.py
@@ -5,10 +5,18 @@ import matplotlib.pyplot as plt
 from sklearn import metrics
 
 
-def main():
+def one_cluster():
+    main(10, 1, 'dbscan_one_cluster')
+
+
+def best_fit():
+    main(1.7, 10, 'dbscan_best_fit')
+
+
+def main(eps, min_samples, file_name):
     data_points = np.asarray(pandas.read_csv('./data/Dataset2.csv').values.tolist())
     X = data_points
-    db = DBSCAN(eps=1.7, min_samples=10).fit(X)
+    db = DBSCAN(eps=eps, min_samples=min_samples).fit(X)
     core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
     core_samples_mask[db.core_sample_indices_] = True
     labels = db.labels_
@@ -16,10 +24,9 @@ def main():
     n_noise_ = list(labels).count(-1)
     print('Estimated number of clusters: %d' % n_clusters_)
     print('Estimated number of noise points: %d' % n_noise_)
-    print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(X, labels))
+    # print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(X, labels))
     unique_labels = set(labels)
-    colors = [plt.cm.Spectral(each)
-              for each in np.linspace(0, 1, len(unique_labels))]
+    colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
     for k, col in zip(unique_labels, colors):
         if k == -1:
             col = [0, 0, 0, 1]
@@ -31,8 +38,9 @@ def main():
         plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
                  markeredgecolor='k', markersize=6)
     plt.title('Estimated number of clusters: %d' % n_clusters_)
-    plt.savefig('./figures/dbscan.png')
+    plt.savefig(str('./figures/' + file_name + '.png'))
 
 
 if __name__ == "__main__":
-    main()
+    best_fit()
+    one_cluster()
