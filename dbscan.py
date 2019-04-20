5ca2f67a6c9e3fd88e2ef942e57533eb63fb61ab
mostafa <mostafa.ahmadi1375@yahoo.com>
2019-04-20 12:50:34 +0430

    dbscan algorithm is well done

--- dbscan.py
+++ dbscan.py
@@ -2,23 +2,37 @@ import pandas
 from sklearn.cluster import DBSCAN
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import metrics
 
 
 def main():
     data_points = np.asarray(pandas.read_csv('./data/Dataset2.csv').values.tolist())
-    clustering = DBSCAN(eps=2, min_samples=10).fit(data_points)
-    plt.plot(clustering[0], clustering[1], 'g')
-    '''
-    print(clustering.labels_)
-    xs = [float(x[0]) for x in data_points]
-    ys = [float(x[1]) for x in data_points]
-    for i in range(len(data_points)):
-        if clustering.labels_[i] == -1:
-            plt.plot(xs[i], ys[i], 'g')
-        else:
-            plt.plot(xs[i], ys[i], str(clustering.labels_[i]+1))
-    '''
+    X = data_points
+    db = DBSCAN(eps=1.7, min_samples=10).fit(X)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    labels = db.labels_
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise_ = list(labels).count(-1)
+    print('Estimated number of clusters: %d' % n_clusters_)
+    print('Estimated number of noise points: %d' % n_noise_)
+    print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(X, labels))
+    unique_labels = set(labels)
+    colors = [plt.cm.Spectral(each)
+              for each in np.linspace(0, 1, len(unique_labels))]
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            col = [0, 0, 0, 1]
+        class_member_mask = (labels == k)
+        xy = X[class_member_mask & core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                 markeredgecolor='k', markersize=14)
+        xy = X[class_member_mask & ~core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                 markeredgecolor='k', markersize=6)
+    plt.title('Estimated number of clusters: %d' % n_clusters_)
     plt.savefig('./figures/dbscan.png')
+
 
 if __name__ == "__main__":
     main()
